277ea1adcf5669b392faa6a7b46f4645fa60df49
Yangyuan Chen <506213958@qq.com>
2021-09-06 21:28:51 +0800

    Update OpenCL code

--- benchmarks/unit_tests.py
+++ benchmarks/unit_tests.py
@@ -4,7 +4,7 @@ from code.debug_tools import find_instr
 from code.matrix_tools import csr_to_sellpack
 from code.matrix_tools import random_spmatrix
 from code.matrix_tools import spmatrix_to_csr
-from code.numba_spmv import csr_spmv_multi_thread
-from code.numba_spmv import sliced_ellpack_spmv
+from code.numba_spmv import numba_csr_spmv
+from code.numba_spmv import numba_sliced_ellpack_spmv
 
 
